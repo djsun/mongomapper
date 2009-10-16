7b7665ce6ed53a469e5c2b287e1b2632315fc08e
David James <djames@sunlightfoundation.com>
2009-10-16 13:26:30 -0400

    Demonstrating how namespacing is currently a pain

---
+++ test/functional/associations/test_namespace.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+require 'models'
+
+class NamespaceTest < Test::Unit::TestCase
+  def setup
+    Hollywood::Movie.collection.clear
+    Hollywood::Actor.collection.clear
+    Hollywood::Role.collection.clear
+  end
+  
+  context "Hollywood namespace" do
+    setup do
+      @movie = Hollywood::Movie.create
+      @actor = Hollywood::Actor.create
+      @role = Hollywood::Role.create(
+        :movie_id => @movie.id,
+        :actor_id => @actor.id
+      )
+    end
+
+    should "belongs_to associations" do
+      @role.actor.should == @actor
+      @role.movie.should == @movie
+    end
+  end
+end
